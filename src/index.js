6479e777c32a5022f96a1a6222b7aa137ac8a3eb
Josie Kitter <josie.katt@yahoo.com>
2020-09-28 10:44:04 -0400

    adding index.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+const utilities = {
+	path: require('path')
+};
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(utilities.path.join(__dirname + '/index.html'));
 });
 
 app.listen(3000, function () {
